349fa56e6516a23c0239da17c7fe0d2250f219d0
Tianhang Zheng <tzheng4@buffalo.edu>
2022-03-02 12:12:44 -0500

    minor changes

--- models/encoder.py
+++ models/encoder.py
@@ -170,43 +170,6 @@ class lenet_encoder(nn.Module):
                 return mu + sigma*torch.randn_like(sigma)
 
 
-# class AlexNet(nn.Module):
-#
-#     def __init__(self, num_classes: int = 1000) -> None:
-#         super(AlexNet, self).__init__()
-#         self.features = nn.Sequential(
-#             nn.Conv2d(3, 64, kernel_size=11, stride=4, padding=2),
-#             nn.ReLU(inplace=True),
-#             nn.MaxPool2d(kernel_size=3, stride=2),
-#             nn.Conv2d(64, 192, kernel_size=5, padding=2),
-#             nn.ReLU(inplace=True),
-#             nn.MaxPool2d(kernel_size=3, stride=2),
-#             nn.Conv2d(192, 384, kernel_size=3, padding=1),
-#             nn.ReLU(inplace=True),
-#             nn.Conv2d(384, 256, kernel_size=3, padding=1),
-#             nn.ReLU(inplace=True),
-#             nn.Conv2d(256, 256, kernel_size=3, padding=1),
-#             nn.ReLU(inplace=True),
-#             nn.MaxPool2d(kernel_size=3, stride=2),
-#         )
-#         self.avgpool = nn.AdaptiveAvgPool2d((6, 6))
-#         self.classifier = nn.Sequential(
-#             nn.Dropout(),
-#             nn.Linear(256 * 6 * 6, 4096),
-#             nn.ReLU(inplace=True),
-#             nn.Dropout(),
-#             nn.Linear(4096, 4096),
-#             nn.ReLU(inplace=True),
-#             nn.Linear(4096, num_classes),
-#         )
-#
-#     def forward(self, x: torch.Tensor) -> torch.Tensor:
-#         x = self.features(x)
-#         x = self.avgpool(x)
-#         x = torch.flatten(x, 1)
-#         x = self.classifier(x)
-#         return x
-
 class lstm_encoder(nn.Module):
     def __init__(self, voc_size, embedding_dim, out_dim, hidden_dim=None, drop_rate=0.2, info_model=False):
 
